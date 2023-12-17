157e1cc85eef08ca12d4e2901239d1c28e0bfb9c
Shubham <prakashvermashubham@gmail.com>
2023-12-17 19:02:26 +0530

    Update As5.js

--- As5.js
+++ As5.js
@@ -1,4 +1,4 @@
-                                     JS Assignment 5 - DOM
+                                     // JS Assignment 5 - DOM
 
   // JS DOM Assignment - Hot or Cold-------------------------- https://jsfiddle.net/shubham11_11/d2e5whxn/
   // JS DOM Assignment - JS Validation - Phone Survey Form---- https://jsfiddle.net/shubham11_11/s6ng2z3m/30/
